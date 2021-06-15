fe7006ace32854e44d4bf51d4ca9641a79b2b97f
Ciaran Evans <ciaran@developmentseed.org>
2021-06-15 13:18:01 +0100

    Add example 6

---
+++ .github/actions/action-stations-slack/send_message.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+from slack_sdk import WebClient
+
+
+MESSAGE_FORMAT=(
+    ":rotating_light: *ACTION STATIONS* :rotating_light: \n"
+    "Someone just kicked off a build on `ciaranevans/action-stations`!\n"
+    "You can take a look at the GitHub Actions Workflow by clicking"
+    " <{0}|*HERE*> :sleuth_or_spy:"
+)
+
+
+def send_message(token: str, github_build_url: str, channel: str) -> None:
+    client = WebClient(token=token)
+    message = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": MESSAGE_FORMAT.format(github_build_url)
+			}
+		}
+	]
+    client.chat_postMessage(
+        channel=channel,
+        blocks=message
+    )
+
+if __name__ == "__main__":
+    send_message(sys.argv[1], sys.argv[2], sys.argv[3])
